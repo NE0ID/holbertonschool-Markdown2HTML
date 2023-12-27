3b39c8ef15760d49ba061ef23de5e4737de1acff
NeoV0id <jokerghoul.kpop@gmail.com>
2023-12-27 13:49:04 +0100

    [Modify] exception handling

--- markdown2html.py
+++ markdown2html.py
@@ -8,10 +8,15 @@ import sys
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 3:
-        raise("Usage: ./markdown2html.py README.md README.html")
+    try:
+        if len(sys.argv) != 3:
+            raise InvalidNumberOfArgs
+        elif exists(sys.argv[2]) == False:
+            raise NonExistingFile
+    except InvalidNumberOfArgs as e:
+        print ("Usage: ./markdown2html.py README.md README.html")
         exit (1)
-    if exists(sys.argv[2]) == False:
-        raise("Missing <filename>")
+    except NonExistingFile as e:
+        print("Missing {}".format(sys.argv[2]))
         exit (1)
     exit (0) 
